ad635575075bbaf74602620ed2c346e354df6cd6
CaVaiRs <Unlimi.net@mail.ru>
2024-10-15 23:01:18 +0300

    first commit

--- drf-intro/simple_crud/measurements/models.py
+++ drf-intro/simple_crud/measurements/models.py
@@ -5,8 +5,9 @@ class Project(models.Model):
     """Объект на котором проводят измерения."""
 
     name = models.TextField()
-    latitude = models.FloatField()
-    longitude = models.FloatField()
+    latitude = models.FloatField(null=True)
+    longitude = models.FloatField(null=True)
+    description= models.TextField()
     created_at = models.DateTimeField(
         auto_now_add=True
     )
@@ -19,7 +20,7 @@ class Measurement(models.Model):
     """Измерение температуры на объекте."""
 
     value = models.FloatField()
-    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='measurements') 
     created_at = models.DateTimeField(
         auto_now_add=True
     )
--- drf-intro/simple_crud/simple_crud/urls.py
+++ drf-intro/simple_crud/simple_crud/urls.py
@@ -14,10 +14,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from measurements.views import ProjectViewSet, MeasurementViewSet
 
-# TODO: настройте роутер и подключите `ProjectViewSet` и `MeasurementViewSet`
+
+router = DefaultRouter()
+
+
+# Подключаем ProjectViewSet и MeasurementViewSet к роутеру
+router.register(r'projects', ProjectViewSet)  # Для ProjectViewSet
+router.register(r'measurements', MeasurementViewSet)  # Для MeasurementViewSet
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),  # URL для админки
+    path('api/', include(router.urls)),  # 
 ]
